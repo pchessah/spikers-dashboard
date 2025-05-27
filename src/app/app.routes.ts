712646e469dfa86ba170e3c05de33430597fa332
Paul Chesa <paulchesa1@gmail.com>
2025-05-27 12:56:36 +0300

    chore: remove unused files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,4 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./features/pages/dashboard-page/dashboard-page.component').then(m => m.DashboardPageComponent),
   },
-  {
-    path: 'examples',
-    loadComponent: () => import('./elements/pages/examples/examples.component').then(m => m.ExamplesComponent),
-  },
 ];
